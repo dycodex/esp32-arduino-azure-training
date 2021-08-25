c7023237695496419299fe9140a77d20b54188f3
Alwin Arrasyid <alwin.ridd@gmail.com>
2021-08-25 09:17:54 +0700

    added code that reads temperature and humidity using SHTC3

---
+++ day2/finished/using-shtc3/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+
+#include "Adafruit_SHTC3.h"
+
+Adafruit_SHTC3 shtc3 = Adafruit_SHTC3();
+
+void setup()
+{
+    Serial.begin(115200);
+
+    Serial.println("SHTC3 test");
+    if (!shtc3.begin())
+    {
+        Serial.println("Couldn't find SHTC3");
+        while (1)
+            delay(1);
+    }
+    Serial.println("Found SHTC3 sensor");
+}
+
+void loop()
+{
+    sensors_event_t humidity, temp;
+
+    shtc3.getEvent(&humidity, &temp); // populate temp and humidity objects with fresh data
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degrees C");
+    Serial.print("Humidity: ");
+    Serial.print(humidity.relative_humidity);
+    Serial.println("% rH");
+
+    delay(1000);
+}
